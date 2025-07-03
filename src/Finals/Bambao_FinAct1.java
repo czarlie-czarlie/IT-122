7e4dc243abe3c5818800b4bff5641d586eb9b53e
czarlie-czarlie <157677889+czarlie-czarlie@users.noreply.github.com>
2025-07-03 12:45:33 +0800

    Add bubble sort algorithm

--- src/Finals/Bambao_FinAct1.java
+++ src/Finals/Bambao_FinAct1.java
@@ -7,20 +7,30 @@ public class Bambao_FinAct1 {
         Scanner scanner = new Scanner(System.in);
         int[] numbers = new int[6];
 
-       System.out.println("Enter six random numbers");
-       do {
+       System.out.println("Enter six random integers");
            for (byte i = 0; i < numbers.length; i++) {
-               ++i;
-               System.out.print("Enter #" + i + ": \n");
-               --i;
+               System.out.print("Enter #" + (i + 1) + ": ");
                numbers[i] = Integer.parseInt(scanner.nextLine());
            }
             for (int number : numbers) {
                 if (number == numbers[0])
-                    System.out.println("Original Array:");
+                    System.out.println("\nOriginal Array:");
                 System.out.print(number + " ");
            }
-           break;
-       } while (true);
+           for (byte i = 0; i < numbers.length - 1; i++) {
+               for (byte j = 0; j < numbers.length - 1 - i; j++) {
+                   if (numbers[j] > numbers[j + 1]) {
+                       int temp = numbers[j];
+                       numbers[j] = numbers[j + 1];
+                       numbers[j + 1] = temp;
+                   }
+               }
+           }
+           for (int number: numbers) {
+               if (number == numbers[0])
+                   System.out.println("\nSorted Array:");
+               System.out.print(number + " ");
+           }
+        System.out.println("\nSorted array has been written to sorted_output.txt");
     }
 }
